1d46b4f8408e3a65d3f24db8b1e0a4ad6e1ee1bd
Ireozar Zareoto <i.bartkowski@posteo.de>
2024-08-18 13:29:18 +0200

    Added reCaptcha token reading

--- src/winner.rs
+++ src/winner.rs
@@ -1,3 +1,4 @@
+use fantoccini::{ClientBuilder, Locator};
 use poise::serenity_prelude::Result;
 use reqwest::Error;
 
@@ -7,7 +8,28 @@ pub async fn get_winner(url: String) -> Result<String, Error> {
 }
 
 async fn fetch_html(url: &str) -> Result<String, Error> {
-    let response = reqwest::get(url).await?;
-    let body = response.text().await?;
-    Ok(body)
+    let winner = content(url).await.unwrap();
+    Ok(winner)
+}
+
+async fn content(url: &str) -> Result<String, fantoccini::error::CmdError> {
+    let mut client = ClientBuilder::native()
+        .connect("http://localhost:4444")
+        .await
+        .expect("Failed to create client");
+    client.goto(url).await?;
+    client.execute("grecaptcha.ready(function() {
+            grecaptcha.execute('6LcjdkEgAAAAAKoEsPnPbSdjLkf4bLx68445txKj', {action: 'submit'}).then(function(token) {
+                window.recaptchaToken = token;
+            });
+        });",
+    vec![]).await?;
+    tokio::time::sleep(tokio::time::Duration::from_secs(5)).await;
+    let token = client
+        .execute("return window.recaptchaToken", vec![])
+        .await?
+        .to_string();
+    println!("Extracted reCAPTCHA token: {}", token);
+    client.close().await?;
+    Ok(token)
 }
